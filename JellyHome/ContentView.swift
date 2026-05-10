0cbc8d030eb8cd445a3c555c88e17995ddcde344
MarcusKLY <50311376+MarcusKLY@users.noreply.github.com>
2026-05-10 17:59:46 +0800

    Initial Commit

---
+++ JellyHome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JellyHome
+//
+//  Created by Kam Long Yin on 10/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JellyHome/JellyHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JellyHomeApp.swift
+//  JellyHome
+//
+//  Created by Kam Long Yin on 10/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct JellyHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
